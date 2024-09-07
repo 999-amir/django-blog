bae2e9a5b5ce70c798ece44f0b390843ff559deb
amir mohammad arghavany <amir.arghavoon@gmail.com>
2024-09-07 11:26:03 +0430

    connect nginx with websocket ( debug previous commit )

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -3,7 +3,10 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import django
+django.setup()
 from message import routing
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django_asgi_app = get_asgi_application()
